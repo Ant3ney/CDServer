36edc706bee34145d6c1023595a9b0831c16251b
Anthony Cavuoti <anthonycavuoti@gmail.com>
2022-12-12 19:01:13 +0000

    Add more scafolding to server

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const { exec } = require("child_process");
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the continuous deployment server");
+  res.send("Welcome to the continuous deployment server :)");
 });
 
 app.get("/deploy/marvasBackend", (req, res) => {
   exec("ls -la", (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
+      res.send("error");
       return;
     }
     if (stderr) {
       console.log(`stderr: ${stderr}`);
+      res.send("error");
       return;
     }
+    res.send(stdout);
     console.log(`stdout: ${stdout}`);
   });
-  res.send("In marvasBackend");
 });
 
 app.listen(port, () => {
