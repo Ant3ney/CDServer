d30745fe49b4e09f100c5824a73fb91a1cacc6b1
Anthony Cavuoti <46980442+Ant3ney@users.noreply.github.com>
2025-03-29 06:40:43 +0000

    Remove files

--- delaireENV.js
+++
@@ -1,8 +0,0 @@
-const delAireENV = {
-	PORT: '3003',
-	APP_NAME: 'delaire',
-	REPO: 'https://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@github.com/delairebc/delairebc-auth-server.git',
-	STRIPE_SECRET: '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',
-};
-
-module.exports = delAireENV;
